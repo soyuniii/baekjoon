aa7b3da54305ad52a09a87893f417f71458e3f84
soyun <pe1229@pusan.ac.kr>
2026-03-27 19:57:28 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+def solve():
+    while True:
+        a, b, c = map(int, input().split())
+
+        if a == 0 and b == 0 and c == 0:
+            return
+
+        sides = sorted([a, b, c])
+
+        if sides[0]**2 + sides[1]**2 == sides[2]**2:
+            print("right")
+        else:
+            print("wrong")
+
+
+solve()
