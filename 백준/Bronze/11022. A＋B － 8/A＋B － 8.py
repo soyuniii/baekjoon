24f87a43470994dce1ba08516e1b1ffd8b4b919f
soyun <pe1229@pusan.ac.kr>
2026-04-05 11:44:59 +0900

    [Bronze V] Title: A+B - 8, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+
+    
+def solve():
+    t = int(input())
+    for i in range(t):
+        a, b = map(int, input().split())
+        print(f"Case #{i+1}: {a} + {b} = {a+b}") 
+
+solve()
