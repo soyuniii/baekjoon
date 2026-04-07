2bae74a0cdca266ee8e5305d9942aabf2d4c512f
soyun <pe1229@pusan.ac.kr>
2026-04-07 16:06:27 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+    
+def solve():
+    MOD = 1234567891
+    l = int(input())
+    s = input().strip()
+    nums = []
+    result = 0
+
+    for ch in s:
+        nums.append(ord(ch)-ord('a')+1)
+
+    for i in range(l):
+        result += (nums[i] * (31**i)) % MOD
+    print(result)
+
+
+    
+solve()
